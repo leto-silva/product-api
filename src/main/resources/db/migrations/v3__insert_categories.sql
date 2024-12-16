d04917b59495cf53043d68efcb82f46e5f174979
WellingtonEugenio <wellingtoneugenio@gmail.com>
2024-12-16 15:55:23 -0300

    iniciando trabalhos

---
+++ src/main/resources/db/migrations/v3__insert_categories.sql
@@ -0,0 +1,3 @@
+INSERT INTO products.category(id, nome) VALUES (1 'Eletrônico');
+INSERT INTO products.category(id, nome) VALUES (1 'Móveis');
+INSERT INTO products.category(id, nome) VALUES (1 'Brinquedos');
---
+++ target/classes/db/migrations/v2__create_product_table.sql
@@ -0,0 +1,11 @@
+create schema if not exists products;
+
+create table products.product (
+    id bigserial primary key,
+    product_identifier varchar not null,
+    nome varchar(100) not null,
+    descricao varchar not null,
+    preco float not null,
+    category_id bigint references
+    products.category(id)
+);
